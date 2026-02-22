79112057d7c7cecaa3a4a568072df9ce1034fcb4
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2026-02-22 19:07:48 +0100

    Add default keyword to asyncHandler

--- backend/helpers/asyncHandler.ts
+++ backend/helpers/asyncHandler.ts
@@ -1,6 +1,6 @@
 import { Response, Request, NextFunction } from "express";
 
-export async function handler(fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) {
+export default async function handler(fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) {
   return function (req: Request, res: Response, next: NextFunction) {
     Promise.resolve(fn(req, res, next)).catch(next);
   }
