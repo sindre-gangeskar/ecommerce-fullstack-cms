d68f14ae4a0f5b4482d3ea8f13ce952c4e08926a
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2026-02-23 09:58:52 +0100

    remove error throw in UserService to prevent 404 error

--- backend/services/UserService.ts
+++ backend/services/UserService.ts
@@ -1,4 +1,3 @@
-import { createAndThrowError } from "helpers/utils";
 import { Role } from "models/roles";
 import { User } from "models/users";
 
@@ -6,10 +5,6 @@ export default class UserService {
   static async getByEmail(email: string): Promise<User | null> {
     try {
       const user = await User.findOne({ where: { email }, include: { model: Role } });
-      
-      if (!user)
-        createAndThrowError("Failed to find user with provided email", 404, "UserNotFoundError");
-
       return user;
     } catch (error) {
       console.error(error);
